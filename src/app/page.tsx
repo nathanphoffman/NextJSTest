45309e4935fd48565da43c5aa46720920176a299
nathanphoffman <natephiliphoffman@proton.me>
2026-09-12 16:49:11 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      <h1 className="text-4xl font-semibold text-black dark:text-zinc-50">
+        Hello, World!
+      </h1>
+    </div>
+  );
+}
