2ee1498380875b68e96847a3e9e3311c65ae949b
nathanphoffman <natephiliphoffman@proton.me>
2026-09-18 14:20:11 -0400

    adding test build pipeline for tsc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
 export default function Home() {
+
+  type hello = string | number
+
+  const a : hello = "hello"
+
+  type IsString<T> = T extends string ? number : boolean
+  
+  const ss : IsString<string> = 3
+
+
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
       <h1 className="text-4xl font-semibold text-black dark:text-zinc-50">
